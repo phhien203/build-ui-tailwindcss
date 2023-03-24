bc471f54a1c6b53fcd959af0907f585eb370a509
Pham Huu Hien <phhien203@gmail.com>
2023-03-24 23:11:23 +0700

    scaffold multi panel layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from "react";
-import Message from "./Message";
 
 function App() {
-  return <Message />;
+  return (
+    <div className="flex h-screen text-white">
+      <div className="bg-gray-800 p-4">
+        <div className="w-12 h-12 rounded-full flex items-center justify-center bg-white text-gray-800">
+          TW
+        </div>
+      </div>
+      <div className="flex flex-col w-60 bg-gray-700">
+        <div className="p-4 shadow-md">Tailwind CSS</div>
+        <div className="flex-1 p-4">channels</div>
+      </div>
+      <div className="flex flex-col flex-1 bg-gray-600">
+        <div className="p-4 shadow-md">general</div>
+        <div className="flex-1 p-4">messages</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
