c340aa9f2853156dd94efde939ed573896f4aac7
Pham Huu Hien <phhien203@gmail.com>
2023-03-26 22:07:45 +0700

    make servers area scrollable

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import React from "react";
 function App() {
   return (
     <div className="flex h-screen text-white">
-      <div className="bg-gray-800 p-4">
-        <div className="w-12 h-12 rounded-full flex items-center justify-center bg-white text-gray-800">
-          TW
-        </div>
+      <div className="bg-gray-800 p-3 space-y-2 overflow-y-auto">
+        {[...Array(40)].map((_, i) => (
+          <div className="w-12 h-12 rounded-full flex items-center justify-center bg-white text-gray-800">
+            {i}
+          </div>
+        ))}
       </div>
       <div className="flex flex-col w-60 bg-gray-700">
         <div className="p-4 shadow-md">Tailwind CSS</div>
